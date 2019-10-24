34461615a7370bfdedaa090ab93b1900542b7e9c
Jonathan <mschell@thehackettgroup.com>
2019-10-24 12:20:55 -0300

    working

---
+++ assets/js/scripts.js
@@ -0,0 +1,49 @@
+class SaveWorld{
+
+    constructor(){
+
+    }
+
+    masonry(){
+
+        
+    }
+
+}
+
+$(document).ready(function(){
+
+    var saveWorld = new SaveWorld();
+
+    saveWorld.masonry();
+
+})
+
+// MASONRY
+function resizeGridItem(item){
+    grid = document.getElementsByClassName("grid")[0];
+    rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+    rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+      item.style.gridRowEnd = "span "+rowSpan;
+  }
+  
+  function resizeAllGridItems(){
+    allItems = document.getElementsByClassName("item");
+    for(x=0;x<allItems.length;x++){
+      resizeGridItem(allItems[x]);
+    }
+  }
+  
+  function resizeInstance(instance){
+      item = instance.elements[0];
+    resizeGridItem(item);
+  }
+  
+  window.onload = resizeAllGridItems();
+  window.addEventListener("resize", resizeAllGridItems);
+  
+  allItems = document.getElementsByClassName("item");
+  for(x=0;x<allItems.length;x++){
+    imagesLoaded( allItems[x], resizeInstance);
+  }
